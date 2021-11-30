ca685760a61ee2b6397302e15ede04b553d31545
Diogo Monteiro <diogo.mo.monteiro@ua.pt>
2021-11-30 15:15:34 +0000

    Adds sample data templates

---
+++ webapp/src/pages/MachineListPage/MachineListPage.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+
+const MachineListPage = () => {
+
+    return (
+        <div>
+            <h1>
+                hey
+            </h1>
+        </div>
+    )
+}
+
+export default MachineListPage
