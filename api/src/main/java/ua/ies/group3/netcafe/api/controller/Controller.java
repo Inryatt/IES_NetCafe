7d87bba86e791835091baa8f821468f2edadf28f
Tomé Carvalho <tomecarvalho@ua.pt>
2021-12-15 18:08:03 +0000

    TODO: PUT (update) mappings

--- api/src/main/java/ua/ies/group3/netcafe/api/controller/Controller.java
+++ api/src/main/java/ua/ies/group3/netcafe/api/controller/Controller.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ua.ies.group3.netcafe.api.model.Location;
 import ua.ies.group3.netcafe.api.model.Machine;
+import ua.ies.group3.netcafe.api.model.Software;
 import ua.ies.group3.netcafe.api.model.User;
 import ua.ies.group3.netcafe.api.service.LocationService;
 import ua.ies.group3.netcafe.api.service.MachineService;
+import ua.ies.group3.netcafe.api.service.SoftwareService;
 import ua.ies.group3.netcafe.api.service.UserService;
 
 import javax.validation.Valid;
@@ -17,13 +19,14 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api")
 public class Controller {
-
-
     @Autowired
     private LocationService locationService;
 
     @Autowired
     private MachineService machineService;
+
+    @Autowired
+    private SoftwareService softwareService;
 
     @Autowired
     private UserService userService;
@@ -41,16 +44,41 @@ public class Controller {
         return locationService.saveLocation(location);
     }
 
+
     // Machine
 
+    @GetMapping("/machines")
+    public List<Machine> findAllMachines() {
+        return machineService.findAllMachines();
+    }
+
+    @PostMapping("/machines")
+    public Machine addMachine(@Valid @RequestBody Machine machine) {
+        return machineService.saveMachine(machine);
+    }
+    
+
     @GetMapping("/locations/{id}/machines")
-    public List<Machine> getLocationMachines(@PathVariable(value = "id") Long id) {
+    public List<Machine> findLocationMachines(@PathVariable(value = "id") Long id) {
         Optional<Location> locationOptional = locationService.findLocationById(id);
         if (locationOptional.isPresent()) {
             Location location = locationOptional.get();
             return machineService.findMachinesByLocation(location);
         }
         return new ArrayList<>();
+    }
+
+
+    // Software
+
+    @GetMapping("/softwares")
+    public List<Software> findAllSoftwares() {
+        return softwareService.findAllSoftwares();
+    }
+
+    @PostMapping("/softwares")
+    public Software addSoftware(Software software) {
+        return softwareService.saveSoftware(software);
     }
 
 
