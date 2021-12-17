9816a57ba64ce696c19beaff455123b19ffa45c3
itstar <luciusviniciusf@gmail.com>
2021-12-17 20:06:39 +0000

    creates Json Patch for Patch Request

--- pythonparser/data_parser.py
+++ pythonparser/data_parser.py
@@ -1,15 +1,40 @@
-import pika, sys, os
+from typing import Dict, List
+import pika, sys, os, json, requests
+
+def generatePatch(dic : Dict, patches : List = [], prefix="") -> List:
+    """Returns the JsonPatch of given dictionary."""
+    for key,val in dic.items():
+        new_prefix = prefix+"/"+key
+        if isinstance(val, Dict):
+            generatePatch(val, patches=patches, prefix=new_prefix)
+        else:
+            patches.append({"op": "replace", "path": new_prefix, "value": val})
+    return patches
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='machine_usage')
 
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+    def machine_callback(ch, method, properties, body):
+        machine : Dict = json.loads(body)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+        # Machine is something like this:
+        # {'id': 1, 'usage': {'cpu': 2.27, 'gpu': 4.56, 'ram': 5.01, 
+        # 'disk': 0, 'network_up': 3.17, 'network_down': 2.62, 'temp': 21.22, 
+        # 'programs': [{'id': 1, 'name': 'Adobe Photoshop', 'type': 'work'}], 'status': 1}}
+
+        jspatch = generatePatch(machine)
+        print("jspatch:", jspatch)
+
+        base_url = "localhost:1234/machines/"
+        machine_id = machine.get("id")
+        if machine_id is not None:
+            print("POST (not yet) sent to Machine",machine_id,"!")
+            #r = requests.patch(base_url + machine_id, data = jspatch)
+
+    channel.basic_consume(queue='machine_usage', on_message_callback=machine_callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
@@ -18,7 +43,7 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print(' Interrupted')
         try:
             sys.exit(0)
         except SystemExit:
--- pythonparser/test_sender.py
+++ pythonparser/test_sender.py
@@ -1,11 +1,14 @@
-import pika
+import pika, json
+
+static_machine = {"id": 1, "usage": {"cpu": 2.27, "gpu": 4.56, "ram": 5.01, "disk": 0, "network_up": 3.17, "network_down": 2.62, "temp": 21.22, "programs": [{"id": 1, "name": "Adobe Photoshop", "type": "work"}], "status": 1}}
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='machine_usage')
+print("Machine:", static_machine)
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+channel.basic_publish(exchange='', routing_key='machine_usage', body=json.dumps(static_machine))
+print("Sent 'machine")
 connection.close()
